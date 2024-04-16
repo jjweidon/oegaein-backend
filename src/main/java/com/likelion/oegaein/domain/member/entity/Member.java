bd3ff737520520439e96987de67ec72a0d6c9855
SeolHuiGwan9478 <shg5977@naver.com>
2024-04-17 00:52:29 +0900

    Refactor: member 수정

--- src/main/java/com/likelion/oegaein/domain/member/entity/Member.java
+++ src/main/java/com/likelion/oegaein/domain/member/entity/Member.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
