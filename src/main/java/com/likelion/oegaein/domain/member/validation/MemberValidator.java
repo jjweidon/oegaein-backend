1e1758a2ec6b78390950ee3419d220caf56c79bb
SeolHuiGwan9478 <shg5977@naver.com>
2024-04-18 02:39:04 +0900

    Feat: 대댓글 유효성 검사 기능 구현 완료

--- src/main/java/com/likelion/oegaein/domain/member/validation/MemberValidator.java
+++ src/main/java/com/likelion/oegaein/domain/member/validation/MemberValidator.java
@@ -7,15 +7,20 @@ import org.springframework.stereotype.Component;
 public class MemberValidator {
     private final String IS_OWNER_MATCHING_POST_ERR_MSG = "올바른 매칭글 작성자가 아닙니다.";
     private final String IS_OWNER_COMMENT_ERR_MSG = "올바른 댓글 작성자가 아닙니다.";
+    private final String IS_OWNER_REPLY_ERR_MSG = "올바른 대댓글 작성자가 아닙니다.";
     public void validateIsOwnerMatchingPost(Long authenticatedMemberId, Long matchingPostAuthorId){
         if(!authenticatedMemberId.equals(matchingPostAuthorId)){
             throw new MemberException(IS_OWNER_MATCHING_POST_ERR_MSG);
         }
     }
-
     public void validateIsOwnerComment(Long authenticatedMemberId, Long commentId){
         if(!authenticatedMemberId.equals(commentId)){
             throw new MemberException(IS_OWNER_COMMENT_ERR_MSG);
+        }
+    }
+    public void validateIsOwnerReply(Long authenticatedMemberId, Long replyId){
+        if(!authenticatedMemberId.equals(replyId)){
+            throw new MemberException(IS_OWNER_REPLY_ERR_MSG);
         }
     }
 }
