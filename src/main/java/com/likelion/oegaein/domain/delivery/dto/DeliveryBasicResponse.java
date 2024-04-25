14518390a37b6cf0d302d6ffcc97b48f995d6ce3
조성민 <101984130+sungmin306@users.noreply.github.com>
2024-04-26 01:02:05 +0900

    Feat : 공동배달  CRUD 개발 -> 이미지 적용 전

---
+++ src/main/java/com/likelion/oegaein/domain/delivery/dto/DeliveryBasicResponse.java
@@ -0,0 +1,20 @@
+package com.likelion.oegaein.domain.delivery.dto;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DeliveryBasicResponse {
+    private Long id;
+    private String restaurantName;
+    private int nowApplicants;
+    private int maxApplicants;
+    private boolean like;
+    private LocalDateTime deadLine;
+}
---
+++ src/main/java/com/likelion/oegaein/domain/delivery/dto/deliveryPost/PutDeliveryPostRequest.java
@@ -0,0 +1,13 @@
+package com.likelion.oegaein.domain.delivery.dto.deliveryPost;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class PutDeliveryPostRequest {
+    public Long id;
+
+}
